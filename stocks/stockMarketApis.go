6106845dcffc11da0853513d6f91761825fe1166
Shivam <shivam.kumar@go-jek.com>
2020-09-01 14:17:11 +0530

    added new stocks to daily fetch

--- stocks/stockMarketApis.go
+++ stocks/stockMarketApis.go
@@ -48,36 +48,46 @@ type ASXData struct {
 }
 
 func GetLatestData(symbols [][]string) [][]interface{} {
+	MaxColumns := 0
 	for i := range symbols {
-		req, err := http.NewRequest("GET", "https://www.asx.com.au/asx/1/share/"+symbols[i][2], nil)
-		if err != nil {
-			// handle err
+		if len(symbols[i]) >= 3 {
+			if symbols[i][2] != "" {
+				req, err := http.NewRequest("GET", "https://www.asx.com.au/asx/1/share/"+symbols[i][2], nil)
+				if err != nil {
+					// handle err
+				}
+				req.Header.Set("Cookie", "JSESSIONID=.node205; TS0122d459=01a4bc132057a0c4bd1a60c9baf03882902de0d7c9b210ff7595f6427e3dca77446473becff3acb3e879e2d65e5953d1316e90e748eb5e49336ac73fa2495f38fd1682d6b9")
+
+				resp, err := http.DefaultClient.Do(req)
+				if err != nil {
+					// handle err
+				}
+				defer resp.Body.Close()
+
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				var ASXLatestData ASXData
+				json.Unmarshal(body, &ASXLatestData)
+
+				OpenPriceMap[ASXLatestData.Code] = ASXLatestData.OpenPrice
+				ClosePriceMap[ASXLatestData.Code] = ASXLatestData.LastPrice
+				LatestTradeData = ASXLatestData.LastTradeDate
+				if i > 1 {
+					Columns := len(symbols[i])
+					if Columns > MaxColumns {
+						MaxColumns = Columns
+					}
+				}
+
+			}
 		}
-		req.Header.Set("Cookie", "JSESSIONID=.node205; TS0122d459=01a4bc132057a0c4bd1a60c9baf03882902de0d7c9b210ff7595f6427e3dca77446473becff3acb3e879e2d65e5953d1316e90e748eb5e49336ac73fa2495f38fd1682d6b9")
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			// handle err
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		var ASXLatestData ASXData
-		json.Unmarshal(body, &ASXLatestData)
-
-		OpenPriceMap[ASXLatestData.Code] = ASXLatestData.OpenPrice
-		ClosePriceMap[ASXLatestData.Code] = ASXLatestData.LastPrice
-		LatestTradeData = ASXLatestData.LastTradeDate
-
 	}
 
 	var values [][]interface{}
 	firstHeader := false
 	secondHeader := true
-
 	for i := range symbols {
 		var row []interface{}
 		j := 0
@@ -94,7 +104,7 @@ func GetLatestData(symbols [][]string) [][]interface{} {
 			newDate := LatestTradeData[5:7] + "-" + LatestTradeData[8:10] + "-" + LatestTradeData[0:4]
 			t, err := time.Parse("01-02-2006", newDate)
 			if err != nil {
-				panic(err)
+				fmt.Println(err)
 			}
 			week := t.Weekday().String()
 			row = append(row, week[0:3]+LatestTradeData[5:7]+"-"+LatestTradeData[8:10]+"-"+LatestTradeData[0:4], LatestTradeData[5:7]+"-"+LatestTradeData[8:10]+"-"+LatestTradeData[0:4])
@@ -104,8 +114,18 @@ func GetLatestData(symbols [][]string) [][]interface{} {
 			row = append(row, "O", "C")
 			secondHeader = false
 		} else {
-			row = append(row, fmt.Sprintf("%.3f", OpenPriceMap[symbols[i][2]]), fmt.Sprintf("%.3f", ClosePriceMap[symbols[i][2]]))
-
+			for len(row) < MaxColumns {
+				row = append(row, "")
+			}
+			if len(symbols[i]) >= 3 {
+				if symbols[i][2] != "" {
+					row = append(row, fmt.Sprintf("%.3f", OpenPriceMap[symbols[i][2]]), fmt.Sprintf("%.3f", ClosePriceMap[symbols[i][2]]))
+				} else {
+					row = append(row, "", "")
+				}
+			} else {
+				row = append(row, "", "")
+			}
 		}
 		values = append(values, row)
 	}
@@ -122,7 +142,12 @@ func GenerateFinalDailyAlertsSheet(symbols [][]string) ([][]interface{}, [][]int
 		for j := range symbols[i] {
 			row1 = append(row1, symbols[i][j])
 		}
-		row2 = append(row2, ClosePriceMap[symbols[i][2]])
+		if symbols[i][2] != "" {
+			row2 = append(row2, ClosePriceMap[symbols[i][2]])
+		} else {
+			row2 = append(row2, "")
+		}
+
 		closePriceValues = append(closePriceValues, row2)
 		symbolValues = append(symbolValues, row1)
 	}
